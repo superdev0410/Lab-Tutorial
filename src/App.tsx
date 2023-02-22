2abd1c5f3399c65e556c269e054b9c4ff314f5b8
superdev0410 <superdev0410@protonmail.com>
2023-02-22 15:02:01 -0800

    Create my first compoment

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import ProjectPage from "./projects/ProjectsPage"
 import './App.css';
 
 function App() {
   return (
-    <blockquote cite="AI Dev">
-      Tell me and I forget, teach me and I may remember, involve me and I learn.
-    </blockquote>
+    <div className="container">
+      <ProjectPage />
+    </div>
   );
 }
 
---
+++ src/projects/ProjectsPage.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+
+const ProjectPage: React.FC = () => {
+    return (
+        <>
+        <h1>Projects</h1>
+        </>
+    );
+}
+
+export default ProjectPage;
