94b92eed8cb7d0bae6b2fd174b1a3278d4b8a436
superdev0410 <superdev0410@protonmail.com>
2023-02-22 16:13:51 -0800

    Get MOC data

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -1,10 +1,12 @@
 import React from "react"
+import MOCK_PROJECTS from "./MocProjects"
 
 
 const ProjectPage: React.FC = () => {
     return (
         <>
         <h1>Projects</h1>
+        <pre>{JSON.stringify(MOCK_PROJECTS, null, "")}</pre>
         </>
     );
 }
