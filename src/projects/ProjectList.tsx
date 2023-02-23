d9c40f19ca87cb02cb75953ad0c0bbb7e5846a8a
superdev0410 <superdev0410@protonmail.com>
2023-02-22 18:21:18 -0800

    Display list data

--- src/projects/ProjectList.tsx
+++ src/projects/ProjectList.tsx
@@ -1,12 +1,30 @@
 import Project from "./Project";
 
-
 interface ProjectListProps {
     projects: Project[];
 }
 
 const ProjectList = ( { projects }: ProjectListProps ) => {
-    return <pre>{JSON.stringify(projects, null, " ")}</pre>
+    return (
+        <>
+        <ul className="row">
+            {projects.map((project) => (
+                <div key={project["id"]} className="cols-sm">
+                    <div className="card">
+                        <img src={project["imageUrl"]} />
+                        <section className="section dark">
+                            <h5 className="strong">
+                                <strong>{project["name"]}</strong>
+                            </h5>
+                            <p>{project["description"]}</p>
+                            <p>Budget: {project["budget"].toLocaleString()}</p>
+                        </section>
+                    </div>
+                </div>
+            ))}
+        </ul>
+        </>
+    )
 }
 
 export default ProjectList;
